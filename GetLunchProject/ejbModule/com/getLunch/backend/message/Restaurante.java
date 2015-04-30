bbff8302ad6f7ec76b894793ef731205f9675c96
ericomedeiros <medeiroserico23@gmail.com>
2015-04-30 17:34:05 -0300

    Starting project

---
+++ GetLunchProject/ejbModule/com/getLunch/backend/message/Restaurante.java
@@ -0,0 +1,21 @@
+package com.getLunch.backend.message;
+
+public class Restaurante {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Restaurante(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
